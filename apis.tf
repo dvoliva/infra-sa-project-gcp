4950df80564b49fac60a2c1deecc15763c141a13
dvoliva <venegasolivad@gmail.com>
2025-02-18 00:38:54 -0300

    habilitando api de pub/sub

--- apis.tf
+++ apis.tf
@@ -2,10 +2,11 @@
 resource "google_project_service" "required_apis" {
   for_each = toset([
     "bigquery.googleapis.com",
-    "storage.googleapis.com",              # API de Cloud Storage
-    "iam.googleapis.com",                  # API de IAM
-    "cloudresourcemanager.googleapis.com", # API de Resource Manager
-    "secretmanager.googleapis.com"         # API de Secret Manager
+    "storage.googleapis.com",              
+    "iam.googleapis.com",                  
+    "cloudresourcemanager.googleapis.com", 
+    "secretmanager.googleapis.com",
+    "pubsub.googleapis.com"         # API de Secret Manager
   ])
 
   project                    = var.project_id
