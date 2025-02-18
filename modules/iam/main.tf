2886f15cac42624ae08dd28290ac18a96a572c5f
dvoliva <venegasolivad@gmail.com>
2025-02-18 15:36:46 -0300

    creando cuentas de servicios con archivos json

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,5 +1,5 @@
 locals {
-  service_accounts = jsondecode(file("${path.module}/service_accounts.json"))
+  service_accounts = jsondecode(file("${path.module}/config/service_accounts.json")).service_accounts
 }
 
 resource "google_service_account" "sa" {
