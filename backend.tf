dc4bada86128fab8435096a29428bb63d260f25d
dvoliva <venegasolivad@gmail.com>
2025-02-17 23:35:50 -0300

    ft: agregando step de apply para tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,5 @@ terraform {
   backend "gcs" {
     bucket = "sentiment-analysis-450917-tf-bucket"
     prefix = "terraform/state"
-    #credentials = "./sentiment-analysis-450917-1ff48b5f1891.json"
   }
 }
--- providers.tf
+++ providers.tf
@@ -1,10 +1,5 @@
-#definir provider cloud
-# data "google_secret_manager_secret_version" "terraform_sa" {
-#   secret = "terraform-sa-key"
-# }
 
 provider "google" {
-  # credentials = var.credentials_json_path
   project = var.project_id
   region  = var.region_name
 }
