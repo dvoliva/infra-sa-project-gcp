e2c950312252130b8c2c69f602e15ec0b41ade92
dvoliva <venegasolivad@gmail.com>
2025-02-19 21:49:49 -0300

    corrigiendo cuentas de servicio

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -8,4 +8,9 @@ resource "google_service_account" "sa" {
   display_name = each.value.display_name
 }
 
-
+resource "google_project_iam_member" "sa_roles" {
+  for_each = { for sa in local.service_accounts : sa.name => sa }
+  project = var.project_id
+  role = each.key
+  member = "serviceAccount:${google_service_account.sa[each.value.name].email}"
+}
--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,3 +1,8 @@
 variable "project_id" {
   type = string
 }
+
+variable "service_account" {
+  description = "Map of service account name to their roles"
+  type        = object({ name = string, roles : list(string) })
+}
