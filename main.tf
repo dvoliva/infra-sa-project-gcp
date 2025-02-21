0fe5205d7921374c9037c7291724d8d28fad18bc
dvoliva <venegasolivad@gmail.com>
2025-02-21 16:17:18 -0300

    creando buckets para el flujo de datos

--- main.tf
+++ main.tf
@@ -10,9 +10,21 @@ module "cloud-storage" {
   project_id  = var.project_id
 }
 
+module "cloud-storage2" {
+  source      = "./modules/cloud-storage"
+  bucket_name = [
+    "sentiment-analysis-raw-data",
+    "sentiment-analysis-intermediate-data",
+    "sentiment-analysis-models",
+    "sentiment-analysis-logs"
+  ]
+  region_name = var.region_name
+  project_id  = var.project_id
+}
+
 module "iam" {
-  count = length(local.service_accounts)
-  source     = "./modules/iam"
-  project_id = var.project_id
+  count           = length(local.service_accounts)
+  source          = "./modules/iam"
+  project_id      = var.project_id
   service_account = local.service_accounts[count.index]
 }
--- modules/cloud-storage/main.tf
+++ modules/cloud-storage/main.tf
@@ -10,3 +10,18 @@ resource "google_storage_bucket" "tfstate_bucket" {
     enabled = true
   }
 }
+
+resource "google_storage_bucket" "buckets" {
+  for_each = toset(var.bucket_name)
+
+  name     = each.key
+  location = var.region_name
+
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
